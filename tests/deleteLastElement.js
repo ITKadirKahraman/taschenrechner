8b3b1c989605ae5bd92e6280a8702e4093abb2d2
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-07-19 23:53:13 +0200

    modify file

--- tests/deleteLastElement.js
+++ tests/deleteLastElement.js
@@ -8,18 +8,3 @@ export function deleteLastElementFromCalculator(firstNumber, secondNumber, targe
             break;
     }
 }
-/*
-function deleteLastElement() {
-    if(!isSecondNumber){
-        firstNumber = firstNumber.slice(0 , -1);
-        display.value = firstNumber;
-    }else {
-        secondNumber = secondNumber.slice(0, -1);
-        display.value = secondNumber;
-    }
-}
-
-function deleteLastElement2() {
-    display.value = display.value.slice(0, -1);
-}
-*/
